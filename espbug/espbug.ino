9f00c617c1662b128f3dca8aa37ea84640283101
pratik-londhe4 <pratik.londhe@moderncoe.edu.in>
2022-05-03 14:01:36 +0530

    updated web pages with english translation

--- espbug/espbug.ino
+++ espbug/espbug.ino
@@ -1,92 +1,104 @@
+#include <LiquidCrystal.h>
 
+const int rs = 12, en = 11, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 
-#include <FS.h>
-#include <ArduinoJson.h>
-#include "languages.h"
-#include "config.h"
-#include "functions.h"
-#include "ledmsg.h"
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+#define DT A2
+#define SCK A3
+#define sw 3
 
-#include "filesystem.h"
+long sample=0;
+float val=0;
+long count=0;
 
-
-#include "jsonfiles.h"
-#include "webfiles.h"
-
-#include "servingWebPages.h"
-
-#define LED 2
-
-unsigned long previousMillisLoop = 0;        // will store last time LED was updated
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(LED, OUTPUT);
-
-  digitalWrite(LED, 1);
-  delay(100);
-  digitalWrite(LED, 0);
-
-
-  startSPIFFS();
-  logging("");
-  logging("");
-  logging("------ BOOTING ------");
-  logging("");
-
-  logging("setup :: Initialising File System... Success!");
-
-
-  logging("setup :: Starting LoadConfig...");
-  loadSettingJSON();
-  createSystemJSON();
-  logging("setup :: loadSettingJSON DONE");
-  // put your setup code here, to run once:
-  logging("setup :: Starting copyWebFiles ...");
-  copyWebFiles(true);
-  logging("setup :: copyWebFiles DONE");
-
-  logging("setup :: Starting copyWebFiles ...");
-  startAP();
-  logging("setup :: startAP DONE");
-
-
-  digitalWrite(LED, 1);
-  delay(500);
-  digitalWrite(LED, 0);
-  delay(500);
-  digitalWrite(LED, 1);
-  delay(500);
-  digitalWrite(LED, 0);
-  delay(500);
-  digitalWrite(LED, 1);
-  delay(500);
-  digitalWrite(LED, 0);
-}
-
-void loop() {
-//  ftpSrv.handleFTP();
-  dnsServer.processNextRequest();
-  server.handleClient();
-  httpServer.handleClient();
-  ledmsgvoid();
-
-
-
-  // constants won't change:
-
-
-  unsigned long currentMillisLoop = millis();
-
-  if (currentMillisLoop - previousMillisLoop >= 2000) {
-    previousMillisLoop = currentMillisLoop;
-    client_status();
-
+unsigned long readCount(void)
+{
+  unsigned long Count;
+  unsigned char i;
+  pinMode(DT, OUTPUT);
+  digitalWrite(DT,HIGH);
+  digitalWrite(SCK,LOW);
+  Count=0;
+  pinMode(DT, INPUT);
+  while(digitalRead(DT));
+  for (i=0;i<24;i++)
+  {
+    digitalWrite(SCK,HIGH);
+    Count=Count<<1;
+    digitalWrite(SCK,LOW);
+    if(digitalRead(DT)) 
+    Count++;
   }
-
-
-
-
-
-
+  digitalWrite(SCK,HIGH);
+  Count=Count^0x800000;
+  digitalWrite(SCK,LOW);
+  return(Count);
 }
+
+void setup()
+{
+  pinMode(SCK, OUTPUT);
+  pinMode(sw, INPUT_PULLUP);
+  lcd.begin(16, 2);
+  lcd.print("    Weight ");
+  lcd.setCursor(0,1);
+  lcd.print(" Measurement ");
+  delay(1000);
+  lcd.clear();
+  calibrate();
+}
+void calibrate()
+{
+    lcd.clear();
+  lcd.print("Calibrating...");
+  lcd.setCursor(0,1);
+  lcd.print("Please Wait...");
+  for(int i=0;i<100;i++)
+  {
+    count=readCount();
+    sample+=count;
+  }
+  sample/=100;
+  lcd.clear();
+  lcd.print("Put 100g & wait");
+  count=0;
+  while(count<1000)
+  {
+    count=readCount();
+    count=sample-count;
+  }
+  lcd.clear();
+  lcd.print("Please Wait....");
+  delay(2000);
+  for(int i=0;i<100;i++)
+  {
+    count=readCount();
+    val+=sample-count;
+  }
+  val=val/100.0;
+  val=val/100.0;        // put here your calibrating weight
+  lcd.clear();
+}
+
+void loop()
+{
+  count= readCount();
+  int w=(((count-sample)/val)-2*((count-sample)/val));
+  lcd.setCursor(0,0);
+  lcd.print("Measured Weight");
+  lcd.setCursor(0,1);
+  lcd.print(w);
+  lcd.print("g             ");
+
+  if(digitalRead(sw)==0)
+  {
+    val=0;
+    sample=0;
+    w=0;
+    count=0;
+    calibrate();
+  }
+}
+
+
+    
